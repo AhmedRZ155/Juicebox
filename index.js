1dec69f9b18fda532f8b8bb8ac1fe41e2e6c5636
Ahmed Zarrough <123034431+AhmedRZ155@users.noreply.github.com>
2023-05-22 21:34:42 -0500

    work

--- index.js
+++ index.js
@@ -16,6 +16,10 @@ server.use((req, res, next) => {
   next();
 });
 
+server.get('/', (req, res, next) => {
+  res.send('Hello, Welcome to our API!');
+});
+
 server.use((error, req, res, next) => {
   console.error(error);
   res.status(error.status || 500).send(error.message || "Internal server error.");
